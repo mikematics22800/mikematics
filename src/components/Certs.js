96a61caa83ce5b822cde6d952cbc4ee135d043de
Michael Medina <michaeljmedina22800@gmail.com>
2024-10-29 11:11:25 -0400

    updated refs, certs, and resume

--- src/components/Certs.js
+++ src/components/Certs.js
@@ -1,14 +1,14 @@
 const Certs = () => {
   return (
     <section id="certs">
-      <h1 className="title certs-title" id="certs-title">edX Coding Boot Camp</h1>
-      <h2>Technical Skills Gained:</h2>
+      <h1 className="title certs-title" id="certs-title">edX Coding Boot Camp - Core Concepts</h1>
       <ul>
-        <li>Create full-stack single-page web applications using RESTful API routes and AJAX methods, and describe how front-end applications communicate with back-end applications and databases.</li>
-        <li>Implement structured and unstructured databases to convert static websites into dynamic websites that persist data.</li>
-        <li>Build communication skills and demonstrate the foundational computer-science knowledge that is required during technical interviews.</li>
-        <li>Apply the accepted and standard basics of social coding—including source control, issue tracking, and functional feedback—as part of a development community, while building an application.</li>
-        <li>Demonstrate strong teamwork and project management skills as a collaborator and independent contributor during the development cycle of complex projects.</li>
+        <li>Syntax and logic in languages including Python, JavaScript, and C++</li>
+        <li>Managing and querying relational and non-relational databases including SQL and MongoDB</li>
+        <li>Methods and practices for designing, developing, testing, and maintaining software systems</li>
+        <li>Understanding of computer networking, including protocols, IP addresses, and data transmission</li>
+        <li>Introduction to AI concepts, algorithms, and practical applications of machine learning</li>
+        <li>Fundamentals of protecting computer systems and data from security threats and vulnerabilities</li>
       </ul>
     </section>
   )
